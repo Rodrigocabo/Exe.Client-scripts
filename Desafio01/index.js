19fed30075172b47815ff1cb990b9778ae45cf82
Sousa <rodrigo.a.sousa@capgemini.com>
2024-05-15 23:13:25 -0300

    servicenowt

---
+++ Desafio01/index.js
@@ -0,0 +1,8 @@
+function onLoad() {
+    //Type appropriate comment here, and begin script below
+       var pri = g_form.getValue('priority');
+    if (pri == '1'){
+     alert('este incidente é um incidente p1');
+    }
+ }
+ 
